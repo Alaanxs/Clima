d10811aed59f724a8e608d10a2d550c8d391797c
Alaanxs <alanbrira129@gmail.com>
2020-01-22 19:54:03 -0600

    Mejora de la app con datos del clima de una API

--- app/src/main/java/com/example/clima/MainActivity.kt
+++ app/src/main/java/com/example/clima/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.clima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     var grados:TextView? = null
-    var ciudad:TextView? = null
+    var ciudadd:TextView? = null
     var estatus:TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         grados = findViewById<TextView>(R.id.grados)
-        ciudad = findViewById<TextView>(R.id.ciudad)
+        ciudadd = findViewById<TextView>(R.id.ciudad)
         estatus = findViewById<TextView>(R.id.estatus)
 
-        val ciudades = intent.getStringExtra("com.example.clima.ciudades.CIUDAD")
+        //val ciudades = intent.getStringExtra("com.example.clima.ciudades.CIUDAD")
 
-        val ciudadmx = Ciudad("Ciudad de México", 25, "Nublado")
+        if(Network.hayRed(this)){
+            solicitudHTTPVolley("http://api.openweathermap.org/data/2.5/weather?id=3530597&appid=e4b72ea7c43b8c4ad676e5def05848fd")
+        }
+        else{
+
+        }
+
+        /*val ciudadmx = Ciudad("Ciudad de México", 25, "Nublado")
         val canada = Ciudad("Canada", 2, "Soleado")
 
         if(ciudades == "ciudad-mexico"){
@@ -36,6 +50,30 @@ class MainActivity : AppCompatActivity() {
         }
         else{
             Toast.makeText(this, "Ningun dato", Toast.LENGTH_LONG).show()
-        }
+        }*/
+    }
+
+    private fun solicitudHTTPVolley(url:String){
+        val queue = Volley.newRequestQueue(this)
+
+        val stringRequest = StringRequest( Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                try {
+                    Log.d("solicitudHTTPVolley", response)
+
+                    val gson = Gson()
+                    val ciudad = gson.fromJson(response, Ciudad::class.java)
+                    Log.d("GSON", ciudad.name)
+                    grados?.text = ciudad.main?.temp?.toString()+"°"
+                    ciudadd?.text = ciudad.name
+                    estatus?.text = ciudad.weather?.get(0)?.description
+                }catch (e: Exception){
+
+                }
+            },
+            Response.ErrorListener {  })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 }
